fc7bbefec388bdf06d03c8bb9ec148bcea689fd8
KDekho01 <ksenia.dekhonova@isobar.ru>
2018-02-20 12:45:40 +0300

    init vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="assets/img/header.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: url('assets/img/bg.jpg');
+}
+</style>
---
+++ src/components/app-header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  header {
+    height:300px;
+    width:100%;
+    background: url('./assets/img/bg.jpg') 50% 50%;
+    color: $red;
+  }
+</style>
